31eade7b0570c885317c47989eb5d29c7f9cd4bd
IlhamWidodo <150420688+IlhamWidodo@users.noreply.github.com>
2023-11-27 13:17:57 +0700

    Add files via upload

---
+++ Project final project/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Apotek Murah Sehat</title>
+</head>
+<body>
+    <section>
+        </div>
+        <div class="form-box">
+        <div class="form-value">
+            <form action="">
+                <h2>Masuk</h2>
+                <div class="inputbox">
+                    <ion-icon name="mail-outline"></ion-icon>
+                    <input type="email" placeholder="" required>
+                    <label for="">Email</label>             
+                </div>
+                <div class="inputbox">
+                    <ion-icon name="lock-closed-outline"></ion-icon>
+                    <input type="password" required>
+                    <label for="">Password</label>
+                </div> 
+                <div class="forget">
+                    <label for=""><input type="checkbox">Remember Me <a href="#">Forget Password</a></label>
+                    
+                </div>
+                <button>Login<a href="pages/dashboard.php" type="submit"></button>
+               <!-- <a href="pages/content/dashboard/dashboard" class="btn btn-primary w-100" type="submit">Login</a>-->
+                    
+                <div class="register">
+                    <p><a href="register.php">Don't have an account? register</a></p>
+                </div>     
+            </form>
+    </section>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.2.5/dist/ionicons/ionicons.js"></script>
+</body>
---
+++ Project final project/pages/register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">  
+    <title>Document</title>
+</head>
+<body>
+    <div class="register2">
+        <h2>REGISTER</h2>
+            <input type="text" name="name" placeholder="Enter your name">
+            <input type="email" name="email" placeholder="Enter your email">
+            <input type="password" name="password" placeholder="Enter your password">
+            <input type="password" name="confirm" placeholder="Confirm your password">
+            <input class="submit" type="submit" value="Register" name="submit">
+            <p>Already have an account? <a href="login.php">Login</a></p>
+        </div>
+    
+</body>
+</html>
