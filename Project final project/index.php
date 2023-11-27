e3eec16b7b900435d9766735eddd691b7282a9c8
Rizky-pixel <56118223+Rizky-pixel@users.noreply.github.com>
2023-11-27 20:51:33 +0700

    Add files via upload

--- Project final project/index.php
+++ Project final project/index.php
@@ -5,34 +5,35 @@
     <title>Apotek Murah Sehat</title>
 </head>
 <body>
-    <section>
-        </div>
+    <section class="login-section">
         <div class="form-box">
-        <div class="form-value">
-            <form action="">
-                <h2>Masuk</h2>
-                <div class="inputbox">
-                    <ion-icon name="mail-outline"></ion-icon>
-                    <input type="email" placeholder="" required>
-                    <label for="">Email</label>             
-                </div>
-                <div class="inputbox">
-                    <ion-icon name="lock-closed-outline"></ion-icon>
-                    <input type="password" required>
-                    <label for="">Password</label>
-                </div> 
-                <div class="forget">
-                    <label for=""><input type="checkbox">Remember Me <a href="#">Forget Password</a></label>
-                    
-                </div>
-                <button>Login<a href="pages/dashboard.php" type="submit"></button>
-               <!-- <a href="pages/content/dashboard/dashboard" class="btn btn-primary w-100" type="submit">Login</a>-->
-                    
-                <div class="register">
-                    <p><a href="register.php">Don't have an account? register</a></p>
-                </div>     
-            </form>
+            <div class="form-value">
+                <form action="./backend/login.php" method="post">
+                    <h2>Masuk</h2>
+                    <div class="inputbox">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="email" name="email" placeholder="" required>
+                        <label for="">Email</label>             
+                    </div>
+                    <div class="inputbox">
+                        <ion-icon name="lock-closed-outline"></ion-icon>
+                        <input type="password" name="password" required>
+                        <label for="">Password</label>
+                    </div> 
+                    <div class="forget">
+                        <label for=""><input type="checkbox">Remember Me <a href="#">Forget Password</a></label>
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+                <form action="./pages/register.php" method="post">
+                    <div class="register">
+                        <p><a href="./pages/register.php">Don't have an account? Register</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
     </section>
-<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@5.2.5/dist/ionicons/ionicons.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.2.5/dist/ionicons/ionicons.js"></script>
 </body>
+</html>
